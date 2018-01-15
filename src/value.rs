c7d1258b1d75dda55ac20db7b260b13e43f854c9
losfair <zhy20000919@hotmail.com>
2018-01-15 20:37:33 +0800

    Value types

--- src/value.rs
+++ src/value.rs
@@ -2,8 +2,10 @@ use executor::ExecutorImpl;
 
 pub enum Value {
     Object(usize),
-    Int64(i64),
-    Float64(f64)
+    Null,
+    Bool(bool),
+    Int(i64),
+    Float(f64)
 }
 
 pub struct ValueContext<'a> {
@@ -22,16 +24,46 @@ impl<'a> ValueContext<'a> {
     pub fn to_i64(&self) -> i64 {
         match *self.value {
             Value::Object(id) => self.executor.get_object_pool().get_direct(id).to_i64(),
-            Value::Int64(v) => v,
-            Value::Float64(v) => v as i64
+            Value::Null => 0,
+            Value::Bool(v) => if v {
+                1
+            } else {
+                0
+            },
+            Value::Int(v) => v,
+            Value::Float(v) => v as i64
         }
     }
 
     pub fn to_f64(&self) -> f64 {
         match *self.value {
             Value::Object(id) => self.executor.get_object_pool().get_direct(id).to_f64(),
-            Value::Int64(v) => v as f64,
-            Value::Float64(v) => v
+            Value::Null => 0.0,
+            Value::Bool(v) => if v {
+                1.0
+            } else {
+                0.0
+            },
+            Value::Int(v) => v as f64,
+            Value::Float(v) => v
+        }
+    }
+
+    pub fn to_bool(&self) -> bool {
+        match *self.value {
+            Value::Object(id) => self.executor.get_object_pool().get_direct(id).to_bool(),
+            Value::Null => false,
+            Value::Bool(v) => v,
+            Value::Int(v) => if v != 0 {
+                true
+            } else {
+                false
+            },
+            Value::Float(v) => if v != 0.0 {
+                true
+            } else {
+                false
+            }
         }
     }
 }
